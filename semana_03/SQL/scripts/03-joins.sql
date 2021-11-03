b063ed6a7b2ee282a05bcb7aaecb98099cf65938
ras <agalvezcorell@gmail.com>
2021-11-03 13:35:33 +0100

    SQL_done_🐋

--- semana_03/SQL/scripts/03-joins.sql
+++ semana_03/SQL/scripts/03-joins.sql
@@ -1,10 +1,18 @@
 /* Queremos sacar de la tabla los libros con sus autores, teniendo en cuenta que tenemos la información diversificada
 en tres tablas diferentes...entendamos el camino e intentemos hacerlo */
+SELECT t.title, t.title_id, a.au_id, CONCAT(au_fname, " ", au_lname) as full_name
+FROM titles AS t
+LEFT JOIN titleauthor as a
+ON t.title_id = a.title_id
+LEFT JOIN authors as au
+ON a.au_id = au.au_id;
 
-
-
-
-
+SELECT titles.title, titles.title_id, titleauthor.au_id, CONCAT(au_fname, " ", au_lname) as full_name
+FROM titles
+LEFT JOIN titleauthor
+ON titles.title_id = titleauthor.title_id
+LEFT JOIN authors
+ON titleauthor.au_id = authors.au_id;
 
 
 
@@ -12,10 +20,16 @@ en tres tablas diferentes...entendamos el camino e intentemos hacerlo */
 Unámoslos y obtengamos un recuento del número de títulos que ha publicado cada editorial. */
 
 
+SELECT pub_name, title
+FROM publishers
+INNER JOIN titles
+ON titles.pub_id = publishers.pub_id;
 
-
-
-
+SELECT pub_name, COUNT(title)
+FROM publishers
+INNER JOIN titles
+ON titles.pub_id = publishers.pub_id
+GROUP BY pub_name;
 
 
 /* Como hemos utilizado un INNER JOIN para esta consulta, sólo devuelve los resultados de las editoriales cuyo pub_id está en ambas tablas. 
@@ -25,7 +39,11 @@ Todo lo que tenemos que hacer es sustituir nuestro INNER JOIN por un LEFT JOIN y
 de editoriales y contará con cero registros para las que no tienen ningún título publicado.*/
 
 
-
+SELECT pub_name, COUNT(title)
+FROM publishers
+LEFT JOIN titles
+ON titles.pub_id = publishers.pub_id
+GROUP BY pub_name;
 
 
 
